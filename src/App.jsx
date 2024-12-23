0e6fd843dec54e8114b1e4ba3d22967db5c425c7
Souravroy99 <souravroy200418@gmail.com>
2024-12-23 21:03:09 +0530

    Host link at App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import About from "./components/About"
 import Features from "./components/Features"
 import Story from "./components/Story"
 
+// https://anime-website-react-m7ni.onrender.com/
+
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
